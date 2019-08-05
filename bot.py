448f3396ed66c890e1619adad355013890ff2101
Халион <xalion67@gmail.com>
2019-08-05 17:21:18 +0300

    delete spare code

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import shoper
 import telebot as tb
 from time import sleep
 from core import loger, proxy_changer
-from core import database_connector as db_connect
 
 
 working_proxy = proxy_changer.read_proxy()
@@ -44,9 +43,6 @@ def response_to_user(message):
     if action == 'purchase_name':
         list_name = response_json['result']['parameters']['list_name']
         bot.send_message(chat_id, f'Назову список {list_name}\r\nЧто будем покупать?')
-        connection, cursor = db_connect.connect()
-        sql_request = 'INSERT INTO purchase(purchase_name) VALUES (?);'
-        cursor.execute(sql_request, (list_name,))
     elif action == 'purchase_list':
         # Достаём список покупок от пользователя
         purchase_list_from_ai = response_json['result']['parameters']['list'][0]

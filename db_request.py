1ae492a7fd87eba484f0b482ef2faa5040251ab1
Xalion <xalion67@gmail.com>
2020-06-01 21:07:03 +0300

    test

--- db_request.py
+++ db_request.py
@@ -13,19 +13,19 @@ def write_purchase(title, purchases, chat_id):
         cursor.execute(sql_request, (title, purchases, chat_id))
         connection.commit()
 
-def read_purchase(self, chat_id):
+def read_purchase(chat_id):
     connection, cursor = connect()
     sql_request = 'SELECT purchase_name, purchase_list FROM purchase WHERE id=?;'
     cursor.execute(sql_request, (chat_id, ))
     print(cursor.fetchall())
 
-def update_purchase(self, purchase_string, chat_id):
+def update_purchase(purchase_string, chat_id):
     connection, cursor = connect()
     sql_request = 'UPDATE purchase SET purchase_name=?, purchase_list=? WHERE id=?;'
     cursor.execute(sql_request, (self.title, self.purchases, chat_id))
     connection.commit()
 
-def delete_purchase(self, chat_id):
+def delete_purchase(chat_id):
     connection, cursor = connect()
     sql_request = 'DELETE FROM purchase WHERE id=?;'
     cursor.execute(sql_request, (chat_id,))

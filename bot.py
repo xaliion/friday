ff96e077a94cdd615cd0d9617b8c8bfc802ce68d
Xalion <xalion67@gmail.com>
2021-05-12 14:42:14 +0300

    remove df func and change logic, add button

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from dialogflow import df
+from telebot import types
 import telebot as tb
 import configparser
 import logging
@@ -22,40 +22,48 @@ def say_hello(message):
 Я помогаю со списками покупок или дел.
 Чтобы создать список, попроси меня об этом и перечисли, что нужно купить или сделать через запятую.
 Например вот так: что-то, ещё что-то, и ещё что-то'''.format(message.from_user.first_name)
-    bot.send_message(message.chat.id, about_me)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    create_new_list_button = types.KeyboardButton('Новый список')
+    keyboard.add(create_new_list_button)
+    bot.send_message(message.chat.id, about_me, reply_markup=keyboard)
 
 
-@bot.message_handler(content_types=['text'])
-def response_to_user(message):
-    response = df.request_to_dialogflow(df.collect_request(message.text))
-    if df.action(response) == 'set_purchase_list':
-        item_list = df.parameters(response)['purchase_list']
-        user_item_list = shoper.Purchases(purchases=item_list)
-        user_item_list.save_purchase(message.chat.id)
-        users_purchases_data[message.chat.id] = user_item_list
-        keyboard = user_item_list.create_inline_keyboard()
-        bot.send_message(message.chat.id, f'{df.response_ai(response)}', reply_markup=keyboard)
-    elif df.action(response) == 'purchase_reminder':
-        datetime_remind_from_ai = df.parameters(response)
-        if not datetime_remind_from_ai['time']:
-            bot.send_message(message.chat.id, 'Не могу прочитать время')
-        else:
-            purchases = users_purchases_data[message.chat.id]
-            datetime_reminder = purchases.create_reminder(datetime_remind_from_ai, message.chat.id)
-            message_to_recap = purchases.set_reminder(datetime_reminder, bot, message.chat.id)
-            bot.send_message(message.chat.id, message_to_recap)
-    elif df.action(response) == 'send_log':
-        user_id = '{}'.format(message.from_user.id)
-        hex_user_id = hashlib.sha256(user_id.encode('utf-8')).hexdigest()
-        if hex_user_id in config['permissions']['log_permissions']:
-            log = open('./bot.log')
-            bot.send_document(message.chat.id, log, caption='Держи логи')
-            bot_logger.info('issuing a log to a user with access, user – {}'.format(message.from_user.username))
-        else:
-            bot_logger.warning('request a log from a user without access, user – {}'.format(message.from_user.username))
-            bot.send_message(message.chat.id, 'У вас нет доступа к логу')
+@bot.message_handler(regexp='Новый список')
+def create_list(message):
+    msg_obj = bot.send_message(
+        message.chat.id,
+        'Записываю что покупать, главное пиши через запятую',
+        )
+    bot.register_next_step_handler(msg_obj, parse_list)
+
+
+def parse_list(message):
+    user_item_list = shoper.Purchases(purchases=message.text)
+    user_item_list.save_purchase(message.chat.id)
+    users_purchases_data[message.chat.id] = user_item_list
+    keyboard = user_item_list.create_inline_keyboard()
+    bot.send_message(message.chat.id, 'Готово', reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['log'])
+def send_log(message):
+    user_id = str(message.from_user.id)
+    hex_user_id = hashlib.sha256(user_id.encode('utf-8')).hexdigest()
+    if hex_user_id in config['permissions']['log_permissions']:
+        log = open('./bot.log')
+        bot.send_document(message.chat.id, log, caption='Держи логи')
+        bot_logger.info(
+            'issuing a log to a user with access, user – {}'.format(
+                message.from_user.username,
+                ),
+            )
     else:
-        bot.send_message(message.chat.id, f'{df.response_ai(response)}')
+        bot_logger.warning(
+            'request a log from a user without access, user – {}'.format(
+                message.from_user.username,
+                ),
+            )
+        bot.send_message(message.chat.id, 'У вас нет доступа к логу')
 
 
 @bot.callback_query_handler(lambda query: True)
@@ -65,10 +73,15 @@ def delete_button_from_list(query):
     try:
         purchases = users_purchases_data[chat_id]
     except KeyError:
-        bot_logger.error('restore list after reconnecting, user – {}'.format(query.message.from_user.username))
+        bot_logger.error(
+            'restore list after reconnecting, user – {}'.format(
+                query.message.from_user.username,
+                ),
+            )
         purchases = shoper.get_purchases(chat_id)
         users_purchases_data[chat_id] = purchases
     inline_keyboard = purchases.edit_purchase(query, chat_id)
+    bot.answer_callback_query(query.id, 'Убрала из списка')
     if not inline_keyboard.keyboard:
         purchases.delete_purchase(bot, chat_id, query)
     else:
@@ -78,5 +91,5 @@ def delete_button_from_list(query):
 
 try:
     bot.polling()
-except:
+except OSError:
     bot_logger.exception('Disconnected, restart bot')

e8e8f0a3dafb91003f76140e2d2158c95e6c3fff
Xalion <xalion67@gmail.com>
2021-05-18 00:20:42 +0300

    rename some variables, func and pretty text output

--- bot.py
+++ bot.py
@@ -10,18 +10,24 @@ config = configparser.ConfigParser()
 config.read('./config.ini')
 bot = tb.TeleBot(config['bot']['token'], threaded=False)
 bot_logger = logging.getLogger(config['logger']['title'])
-logging.basicConfig(filename=config['logger']['filename'], level=logging.INFO,
-                    format=config['logger']['text_format'],
-                    datefmt=config['logger']['date_format'])
+logging.basicConfig(
+    filename=config['logger']['filename'],
+    level=logging.INFO,
+    format=config['logger']['text_format'],
+    datefmt=config['logger']['date_format'],
+    )
 users_purchases_data = {}
 
 
 @bot.message_handler(commands=['start'])
 def say_hello(message):
-    about_me = '''Привет, {}.
-Я помогаю со списками покупок или дел.
-Чтобы создать список, попроси меня об этом и перечисли, что нужно купить или сделать через запятую.
-Например вот так: что-то, ещё что-то, и ещё что-то'''.format(message.from_user.first_name)
+    about_me = (
+        'Привет, ' + message.from_user.first_name + '.\n'
+        'Я помогаю со списками покупок или дел.\n'
+        'Чтобы создать список – нажми на кнопку снизу'
+        'и перечисли, что нужно купить или сделать через запятую.\n'
+        'Например вот так: что-то, ещё что-то, и ещё что-то'
+    )
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     create_new_list_button = types.KeyboardButton('Новый список')
     keyboard.add(create_new_list_button)
@@ -29,20 +35,24 @@ def say_hello(message):
 
 
 @bot.message_handler(regexp='Новый список')
-def create_list(message):
+def write_items(message):
     msg_obj = bot.send_message(
         message.chat.id,
-        'Записываю что покупать, главное пиши через запятую',
+        'Записываю, главное пиши через запятую',
         )
-    bot.register_next_step_handler(msg_obj, parse_list)
+    bot.register_next_step_handler(msg_obj, make_items_list)
 
 
-def parse_list(message):
+def make_items_list(message):
     user_item_list = shoper.Purchases(purchases=message.text)
     user_item_list.save_purchase(message.chat.id)
     users_purchases_data[message.chat.id] = user_item_list
     keyboard = user_item_list.create_inline_keyboard()
-    bot.send_message(message.chat.id, 'Готово', reply_markup=keyboard)
+    bot.send_message(
+        message.chat.id,
+        'Готово.\nВот список:',
+        reply_markup=keyboard,
+        )
 
 
 @bot.message_handler(commands=['log'])
@@ -67,26 +77,29 @@ def send_log(message):
 
 
 @bot.callback_query_handler(lambda query: True)
-def delete_button_from_list(query):
+def delete_item(query):
     chat_id = query.message.chat.id
-    purchases = None
+    items = None
     try:
-        purchases = users_purchases_data[chat_id]
+        items = users_purchases_data[chat_id]
     except KeyError:
         bot_logger.error(
             'restore list after reconnecting, user – {}'.format(
                 query.message.from_user.username,
                 ),
             )
-        purchases = shoper.get_purchases(chat_id)
-        users_purchases_data[chat_id] = purchases
-    inline_keyboard = purchases.edit_purchase(query, chat_id)
-    bot.answer_callback_query(query.id, 'Убрала из списка')
+        items = shoper.get_rebuilt_purchases(chat_id)
+        users_purchases_data[chat_id] = items
+    inline_keyboard = items.edit_purchase(query, chat_id)
+    bot.answer_callback_query(query.id, 'Теперь одним меньше')
     if not inline_keyboard.keyboard:
-        purchases.delete_purchase(bot, chat_id, query)
+        items.delete_purchase(bot, chat_id, query)
     else:
-        bot.edit_message_reply_markup(chat_id, query.message.message_id,
-                                      reply_markup=inline_keyboard)
+        bot.edit_message_reply_markup(
+            chat_id,
+            query.message.message_id,
+            reply_markup=inline_keyboard,
+            )
 
 
 try:
